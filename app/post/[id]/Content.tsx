74681b3770f87d019db25615d3e016a2bed2e549
Dmitry Sky <dmitry.sky@icloud.com>
2023-04-28 11:43:14 -0600

    Toggle Edit text icons

--- app/post/[id]/Content.tsx
+++ app/post/[id]/Content.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { FormattedPost } from "@/app/types";
+import { XMarkIcon, PencilSquareIcon } from "@heroicons/react/24/solid";
 
 type Props = {
     post: FormattedPost;
@@ -17,7 +18,23 @@ const Content = ({ post }: Props) => {
         <div className="prose w-full max-w-full mb-10">
             {/* Breadcrumbs */}
             <h5 className="text-wh-300">{`Home > ${post.category} > ${post.title}`}</h5>
-            Content Component
+            {/* Category and edit icon */}
+            <div className="flex justify-between items-center">
+                <h4 className="bg-accent-orange py-2 px-5 text-wh-900 text-sm font-bold">
+                    {post.category}
+                </h4>
+                <div className="mt-4">
+                    {isEditable ? (
+                        <button onClick={() => setIsEditable(false)}>
+                            <XMarkIcon className="h-6 w-6 text-accent-red" />
+                        </button>
+                    ) : (
+                        <button onClick={() => setIsEditable(true)}>
+                            <PencilSquareIcon className="h-6 w-6 text-accent-red" />
+                        </button>
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
